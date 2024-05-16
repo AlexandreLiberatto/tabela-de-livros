8fba8e53352d3ca99721a9f7421720f7bb39533c
Alexandre Liberato <alexandreliberatto@gmail.com>
2024-05-16 20:09:42 -0300

    Acrescentando setinhas

--- src/components/TabelaHead.jsx
+++ src/components/TabelaHead.jsx
@@ -5,7 +5,12 @@ const TabelaHead = () => {
     
         <thead>
             <tr>
-                <th colSpan='4'>Tabela de Livros</th>
+                <th colSpan='4'>Tabela de Livros
+                <div className="container-setinhas">
+                    <div>&#129093;</div>
+                    <div>&#129095;</div>
+                </div>
+                </th>
             </tr>
             <tr>
                 <th>ISBN</th>
